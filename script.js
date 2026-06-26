6ab9698971eccb72895a932b34b06eb5fc698410
Pushpraj pandey <chunmunpandey444@gmail.com>
2026-06-26 18:01:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let budget = 40000;
+let tvprice =50000;
+let hasspaceinroom = true;
+
+if (tvprice <= budget && hasspaceinroom === true) {
+    console.log("You can buy the TV!");
+} else {
+    console.log("You cannot buy the TV.");
+}
